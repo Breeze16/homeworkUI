e0cc8e97bfca3d0101ff67228f0c9dde67757586
Breeze16 <37762021+Breeze16@users.noreply.github.com>
2018-06-05 17:42:24 +0800

    Create Button_Move.cs

---
+++ Button_Move.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class Button_Move : MonoBehaviour {  
+    public GameObject Panel;  
+    private float cur_x = 0; 
+    private float cur_y = 140;  
+    void Update() {
+        Panel.transform.Find("Image1").gameObject.GetComponent<RectTransform>().localPosition = new Vector3(cur_x, cur_y - 55, 0);
+    }
+}  
