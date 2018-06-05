1fe084557f64dfebe87573f68790924f4f66b321
Breeze16 <37762021+Breeze16@users.noreply.github.com>
2018-06-05 17:41:41 +0800

    Create board.cs

---
+++ board.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class Board : MonoBehaviour {
+    public bool Button = false;
+}
